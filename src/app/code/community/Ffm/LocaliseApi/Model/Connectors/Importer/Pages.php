5a27e9c1914c0d8ae8212b788c02502d8166336d
peterjaap <peterjaap@elgentos.nl>
2016-02-02 13:50:30 +0100

    Rewrote major part of Ffm_LocaliseApi_Model_Connectors_Importer_Pages to have it work with default Magento (Branch: develop)

--- src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Pages.php
+++ src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Pages.php
@@ -30,63 +30,99 @@ class Ffm_LocaliseApi_Model_Connectors_Importer_Pages extends Ffm_LocaliseApi_Mo
         $data = $this->_getTranslatePackage($locale);
 
         $pageData = [];
-        foreach ($data as $identifier => $content) {
-            list($pageId, $type) = explode('..', $identifier);
-            if (!isset($pageData[$pageId])) {
-                $pageData[$pageId] = [];
-            }
-
-            $pageData[$pageId][$type] = $content;
+        foreach ($data as $pageId => $content) {
+            $pageData[$pageId] = array_pop($content);
 
             if (
                 isset($pageData[$pageId]['title']) &&
-                isset($pageData[$pageId]['content heading']) &&
                 isset($pageData[$pageId]['content'])
             ) {
                 $this->_savePage($storeId, $pageId, $pageData[$pageId]);
                 unset($pageData[$pageId]);
+            } else {
+                \Mage::log('Not enough data to save this page');
             }
         }
     }
 
     protected function _savePage($storeId, $pageId, $data)
     {
-        $page = Mage::getModel('cms/page');
-        //Get store Parent
-        $collectionPrnt = $page->getCollection()
-            ->addFieldToFilter('parent_id', 0)
-            ->addFieldToFilter('store_id', $storeId);
+        $pageModel = Mage::getModel('cms/page');
 
-        $parentId = ($collectionPrnt->getSize()) ? $collectionPrnt->getFirstItem()->getId() : 0;
+        if(Mage::helper('core')->isModuleEnabled('Jr_CleverCms')) {
+            //Get store Parent
+            $collectionPrnt = $pageModel->getCollection()
+                ->addFieldToFilter('parent_id', 0)
+                ->addFieldToFilter('store_id', $storeId);
 
-        // check if page exists
-        $collection = $page->getCollection()
-            ->addFieldToFilter('identifier', $pageId);
+            $parentId = ($collectionPrnt->getSize()) ? $collectionPrnt->getFirstItem()->getId() : 0;
+        }
 
-        $incrId = ($collection->getSize()) ? $collection->getFirstItem()->getId() : 0;
+        $originStore = 1;
 
-        if ($incrId) { // new page?
-            $page->load($incrId);
-        } else {
-            $page->addData([
-                'i8l_identifier' => $pageId,
-                'identifier' => $pageId,
-                'store_id' => $storeId,
-                'is_active' => 0,
-                'parent_id' => $parentId,
-            ]);
+        // check if original page exists
+        $collection = $pageModel->getCollection()
+            ->addFieldToSelect('*')
+            ->addFieldToFilter('identifier', $pageId)
+            ->addStoreFilter($originStore);
 
-            if ($storeId === 0 && !Mage::app()->isSingleStoreMode()) {
-                $page->setStores(array_keys(Mage::app()->getStores()));
+        $page = ($collection->getSize()) ? $collection->getFirstItem() : $pageModel;
+
+        if($page->getId() && $storeId == $originStore) {
+            // Leave $page var, it contains the correct page
+        } elseif($page->getId() && $storeId != $originStore) {
+            // Find the page that belongs to this store
+            $collection = $pageModel->getCollection()
+                ->addFieldToSelect('*')
+                ->addFieldToFilter('identifier', $pageId)
+                ->addStoreFilter($storeId);
+
+            if($collection->getSize()) {
+                // Unset ID to save it into the new store
+                $page = $collection->getFirstItem();
+            } else {
+                // Remove page_id to create new page
+                $page->unsetData('page_id');
             }
         }
 
+        $newPageData = [
+            'i8l_identifier' => $pageId,
+            'identifier' => $pageId,
+            'store_id' => $storeId,
+            'is_active' => 0
+        ];
+
+        if(Mage::helper('core')->isModuleEnabled('Jr_CleverCms') && isset($parentId)) {
+            $newPageData['parent_id'] = $parentId;
+        }
+
+        $page->addData($newPageData);
+
         $page->addData([
             'title' => $data['title'],
-            'content_heading' => $data['content heading'],
+            'content_heading' => (isset($data['content_heading']) ? $data['content_heading'] : $data['title']),
             'content' => $data['content'],
+            'meta_description' => (isset($data['meta_description']) ? $data['meta_description'] : null),
+            'identifier' => $data['identifier'],
         ]);
 
-        $page->save();
+        // Do not save pages in All store views
+        if($page->getStoreId() == 0) {
+            return;
+        }
+
+        if(Mage::getIsDeveloperMode()) {
+            \Mage::log('Saving page ' . $page->getIdentifier() . ' in store ' . $storeId . ' ' . $page->getId());
+        }
+
+        try {
+            $page->save();
+        } catch(Exception $e) {
+            if(Mage::getIsDeveloperMode()) {
+                Mage::log('Could not save page ' . $page->getIdentifier() . '; ' . $e->getMessage());
+            }
+            Mage::logException($e);
+        }
     }
 }
