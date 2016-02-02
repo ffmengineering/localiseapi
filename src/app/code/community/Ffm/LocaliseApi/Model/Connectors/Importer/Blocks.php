96a9bf4a8bd3e2bfdf48a88ff014db31997d1596
peterjaap <peterjaap@elgentos.nl>
2016-02-02 14:22:09 +0100

    Fixed block import and raised version to 1.0.13 (Branch: develop)

--- src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Blocks.php
+++ src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Blocks.php
@@ -32,17 +32,19 @@ class Ffm_LocaliseApi_Model_Connectors_Importer_Blocks extends Ffm_LocaliseApi_M
 
         // translate localise data into array
         foreach ($data as $identifier => $attributes) {
-            Mage::log($attributes);continue;
-            foreach (reset($attributes) as $code => $value) {
-                $code = preg_replace('/([^a-z0-9])/i', '_', $code);
+            if(is_array($attributes) && count($attributes)) {
+                foreach (reset($attributes) as $code => $value) {
+                    $code = preg_replace('/([^a-z0-9])/i', '_', $code);
 
-                if (!isset($blocks[$identifier])) $blocks[$identifier] = [];
+                    if (!isset($blocks[$identifier])) $blocks[$identifier] = [];
 
-                $blocks[$identifier][$code] = $value;
-
+                    $blocks[$identifier][$code] = $value;
+                }
             }
         }
-        return;
+
+        // Skip blocks that exist in all store views
+        if($storeId == 0) return;
 
         // fetch the already existing blocks
         $collection = Mage::getModel('cms/block')->getCollection()
@@ -56,7 +58,7 @@ class Ffm_LocaliseApi_Model_Connectors_Importer_Blocks extends Ffm_LocaliseApi_M
 
         // iterate through the retrieved api data
         foreach ($blocks as $identifier => $data) {
-            if (is_null($data['title']) || is_null($data['content'])) // only allow fully translated entities
+            if (is_null($data['title']) || !isset($data['content']) || is_null($data['content'])) // only allow fully translated entities
                 continue;
 
             $_block = Mage::getModel('cms/block')
@@ -72,7 +74,24 @@ class Ffm_LocaliseApi_Model_Connectors_Importer_Blocks extends Ffm_LocaliseApi_M
                 'content' => $data['content'],
             ]);
 
-            $_block->save();
+            try {
+                if($_block->getId()) {
+                    $_block->save();
+                    if(Mage::getIsDeveloperMode()) {
+                        Mage::log('Saved existing block ' . $_block->getIdentifier() . ' / ' . $_block->getId() . ' in store ' . $storeId);
+                    }
+                } else {
+                    $_block->save();
+                    if(Mage::getIsDeveloperMode()) {
+                        Mage::log('Saved new block ' . $_block->getIdentifier() . ' / ' . $_block->getId() . ' in store ' . $storeId);
+                    }
+                }
+            } catch(Exception $e) {
+                if(Mage::getIsDeveloperMode()) {
+                    Mage::log('Could not save block ' . $_block->getIdentifier() . ' in store ' . $storeId . '; ' . $e->getMessage());
+                }
+                Mage::logException($e);
+            }
         }
     }
 }
