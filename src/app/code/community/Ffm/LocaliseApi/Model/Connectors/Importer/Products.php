f365ec0797c8bb1bd18b98d61a56c952a9dd0c58
peterjaap <peterjaap@elgentos.nl>
2016-06-23 14:39:23 +0200

    Fixed Ffm_LocaliseApi to use new array structure with changed API from Localise (Branch: develop)

--- src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Products.php
+++ src/app/code/community/Ffm/LocaliseApi/Model/Connectors/Importer/Products.php
@@ -33,36 +33,19 @@ class Ffm_LocaliseApi_Model_Connectors_Importer_Products extends Ffm_LocaliseApi
         $data = $this->_getTranslatePackage($locale);
         if (!count($data)) return;
 
-        $_product = Mage::getModel('catalog/product');
-        foreach ($data as $identifier => $value) {
-            list($i8l, $attributeCode) = explode('..', $identifier);
-            // localise strips _ & - amongst others
-            $i8l = preg_replace('/([^a-z0-9])/i', '-', $i8l);
-            $attributeCode = preg_replace('/([^a-z0-9])/i', '_', $attributeCode);
-
-            if (!isset($this->_prodCache[$i8l])) {
-                $productId = Mage::getResourceModel('ffm_localiseapi/product')->getIdByI8l($i8l, $websiteId);
-                if (!(int)$productId) {
-                    Mage::log("Could not find product with i8l identifier: {$i8l}", Zend_Log::INFO, 'translation.log', true);
-                    continue;
-                }
-
-                $this->_prodCache[$i8l] = [
-                    'entity_id' => $productId
-                ];
-            }
-
-            $this->_prodCache[$sku][$attributeCode] = $value;
-        }
-
         $_action = Mage::getModel('catalog/resource_product_action');
-        foreach ($this->_prodCache as $i8l => $values) {
-            $productId = $values['entity_id'];
-            unset($values['entity_id']);
+        foreach ($data as $i8l => $values) {
+            $values = array_pop($values);
+
+            if(isset($this->_prodCache[$i8l])) {
+                $productId = $this->_prodCache[$i8l];
+            } else {
+                $productId = Mage::getResourceModel('ffm_localiseapi/product')->getIdByI8l($i8l, $websiteId);
+                $this->_prodCache[$i8l] = $productId;
+            }
 
             try {
                 $_action->updateAttributes(array($productId), $values, $storeId);
-
             } catch(Exception $e) {
                 Mage::log("Error saving product; ID: {$i8l}, error:{$e}", Zend_Log::INFO, 'translation.log', true);
             }
